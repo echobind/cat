aa8b4aae8fe0d0a78e1466eb60956cb202859cfe
Ashley <ashley@echobind.com>
2020-03-27 13:59:15 -0500

    fix issue with api tokens

--- app/helpers/project_epics_helper.rb
+++ app/helpers/project_epics_helper.rb
@@ -1,4 +1,6 @@
 module ProjectEpicsHelper
+    CLUBHOUSE_TEMPLATE_API_TOKEN = ENV['CLUBHOUSE_TEMPLATE_API_TOKEN']
+
     def get_selected_project_epics(project_ids_array)
         all_epics = self.class.get('/epics', :body=> {:token => CLUBHOUSE_TEMPLATE_API_TOKEN})
 
